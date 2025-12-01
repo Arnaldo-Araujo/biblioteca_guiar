dc826bcb6862be01a5960ac700e200a0fda5f16e
Arnaldo Araujo <arnaldoaraujo@unitins.br>
2025-11-30 21:27:06 -0300

    feat: add initial Android application build configuration with Flutter, Firebase, and Java 17 support.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,13 +15,13 @@ android {
 
     compileOptions {
         // 👇 Mude para ficar exatamente assim:
-    isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
